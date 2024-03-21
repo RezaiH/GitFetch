5ae70bc484312012c96203655046dbdc7875734c
hussain <hussain.rasa111@gmail.com>
2024-03-20 23:34:46 -0400

    third commit

--- src/practice/git/SecondClass.java
+++ src/practice/git/SecondClass.java
@@ -5,4 +5,10 @@ public class SecondClass {
 	protected static void mTwo() {
 		System.out.println("This is the first method of class SecondClass.");
 	}
+	private static int showResult () {
+		int num1 = 20;
+		int num2 = 30;
+		int result = num1 + num2;
+		return result;
+	}
 }
