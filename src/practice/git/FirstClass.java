233d7ab3352ba5340956d712f1952188fcec4ecd
RezaiH <117863882+RezaiH@users.noreply.github.com>
2024-03-20 23:24:37 -0400

    Update FirstClass.java

--- src/practice/git/FirstClass.java
+++ src/practice/git/FirstClass.java
@@ -5,4 +5,8 @@ public class FirstClass {
 	private static void mOne() {
 		System.out.println("This is the first void method of class FirstClass.");
 	}
+
+	private static void hussainMethod() {
+		System.out.println("This method is added in the GitHub remote repository.");
+	}
 }
